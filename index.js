14c2ad52b6a67b8db292cee339549f143b51b936
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2018-11-28 11:30:54 +0200

    mend

--- index.js
+++ index.js
@@ -9,6 +9,30 @@
 // Define your objects here
 
 
+class Inhabitant{
+    constructor(specie, name, gender, legs, hands, introduce){
+        this.specie = specie;
+        this.name = `${name.charAt(0).toUpperCase() + name.slice(1)}`;
+        this.gender = gender;
+        this.legs = legs;
+        this.hands = +hands === null ? '0': hands;
+        this.introduce = introduce;
+        this.expression = `<b>General information</b> ( ${specie}, ${gender} ). Hello my name is: <b>${this.name}</b>.  I'm a ,and I have: <b>${this.legs}</b> legs and <b>${this.hands}</b> hands. About myself: <b>${this.introduce}</b>`;
+    }
+}
+
+const man = new Inhabitant('human', 'Maks', 'male', 2, 2, 'I am a drummer.');
+console.log(man);
+const woman = new Inhabitant('human', 'Ira', 'female', 2, 2, 'I am a teacher.');
+const cat = new Inhabitant('animal', 'pushok', 'male', 4, 0, 'I am a cat.');
+const dog = new Inhabitant('animal', 'flash', 'male', 4, 0, 'I am a dog.');
+
+print(man.expression, 'li');
+print(woman.expression, 'li');
+print(cat.expression, 'li');
+print(dog.expression, 'li');
+
+
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
